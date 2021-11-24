453a93c350ecebb92e86121460bfb83d7daf619e
Hen <hentzalah@gmail.com>
2021-11-24 18:16:09 +0200

    Initial commit

---
+++ src/com/hit/algorithm/IAlgoMatchingStrings.java
@@ -0,0 +1,10 @@
+package com.hit.algorithm;
+
+public interface IAlgoMatchingStrings {
+
+    public static void Search() { }
+    public static void computeLPSArray() { }
+    public static int max (int a, int b) { return a; };
+    public static void badCharHeuristic(){}
+
+}
